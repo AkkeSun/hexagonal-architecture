4717a9db5ce5400c08b7c474ac5eb2c56e875e85
od <od@sweettracker.co.kr>
2023-09-14 15:03:34 +0900

    perf: Service 개선

--- src/main/java/com/example/hexagonalarchitecture/application/service/AccountService.java
+++ src/main/java/com/example/hexagonalarchitecture/application/service/AccountService.java
@@ -9,11 +9,12 @@ import com.example.hexagonalarchitecture.application.port.in.account.AccountSear
 import com.example.hexagonalarchitecture.application.port.in.account.WithDrawCommand;
 import com.example.hexagonalarchitecture.application.port.in.account.WithDrawUseCase;
 import com.example.hexagonalarchitecture.application.port.in.account.AccountDTO;
-import com.example.hexagonalarchitecture.application.port.out.LoadAccountPort;
-import com.example.hexagonalarchitecture.application.port.out.LoadUserPort;
-import com.example.hexagonalarchitecture.application.port.out.SaveAccountPort;
+import com.example.hexagonalarchitecture.application.port.out.CreateAccountPort;
+import com.example.hexagonalarchitecture.application.port.out.ReadAccountPort;
+import com.example.hexagonalarchitecture.application.port.out.ReadUserPort;
+import com.example.hexagonalarchitecture.application.port.out.UpdateAccountPort;
 import com.example.hexagonalarchitecture.domain.Account;
-import com.example.hexagonalarchitecture.domain.User;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.ObjectUtils;
 
@@ -27,27 +28,27 @@ import org.springframework.util.ObjectUtils;
 public class AccountService implements DepositUseCase, WithDrawUseCase, AccountSearchUseCase,
     AccountCreateUseCase {
 
-    private final LoadAccountPort loadAccountPort;
-    private final LoadUserPort loadUserPort;
-    private final SaveAccountPort saveAccountPort;
-
+    private final CreateAccountPort createAccountPort;
+    private final UpdateAccountPort updateAccountPort;
+    private final ReadAccountPort loadAccountPort;
+    private final ReadUserPort loadUserPort;
 
     @Override
     public AccountDTO createAccount(AccountCreateCommand command) {
         if(!command.validateSuccess()){
             return AccountDTO.ofFailed("비밀번호와 비밀번호 확인이 올바르지 않습니다");
         }
-        User user;
-        try {
-            user = loadUserPort.getUser(command.getUserIndex());
-        } catch (RuntimeException e) {
+        if(!loadUserPort.existUser(command.getUserIndex())){
             return AccountDTO.ofFailed("존재하지 않는 사용자 정보 입니다");
         }
-        Account newAccount = saveAccountPort.create(Account.builder()
-            .user(user)
+        String accountNum = generateAccountNum(command.getUserIndex());
+        createAccountPort.create(Account.builder()
+            .accountNum(generateAccountNum(command.getUserIndex()))
+            .accountPassword(command.getAccountPassword())
+            .userIndex(command.getUserIndex())
             .money(command.getMoney())
             .build());
-        return AccountDTO.ofSuccess("신규 계좌가 등록되었습니다 - " + newAccount.getAccountNum());
+        return AccountDTO.ofSuccess("신규 계좌가 등록되었습니다 - " + accountNum);
     }
 
     @Override
@@ -66,7 +67,7 @@ public class AccountService implements DepositUseCase, WithDrawUseCase, AccountS
             return AccountDTO.ofFailed("계좌정보가 올바르지 않습니다");
         }
         if(account.withdraw(command.getMoney())) {
-            saveAccountPort.save(account);
+            updateAccountPort.update(account);
             return AccountDTO.ofSuccess("출금이 완료 되었습니다");
         } else {
             return AccountDTO.ofFailed("잔액이 부족합니다");
@@ -80,7 +81,7 @@ public class AccountService implements DepositUseCase, WithDrawUseCase, AccountS
             return AccountDTO.ofFailed("계좌정보가 올바르지 않습니다");
         }
         account.deposit(command.getMoney());
-        saveAccountPort.save(account);
+        updateAccountPort.update(account);
         return AccountDTO.ofSuccess("입금이 완료 되었습니다");
     }
 
@@ -90,6 +91,16 @@ public class AccountService implements DepositUseCase, WithDrawUseCase, AccountS
         } catch (RuntimeException e) {
             return null;
         }
+    }
+
+    private String generateAccountNum(long userIndex) {
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+        String accountNum = String.valueOf(userIndex + (random.nextInt(90000000) + 10000000));
+        if(loadAccountPort.existAccountNum(accountNum)){
+            return generateAccountNum(userIndex);
+        }
+        return accountNum;
     }
 
 }
--- src/main/java/com/example/hexagonalarchitecture/application/service/UserService.java
+++ src/main/java/com/example/hexagonalarchitecture/application/service/UserService.java
@@ -3,27 +3,25 @@ package com.example.hexagonalarchitecture.application.service;
 import com.example.hexagonalarchitecture.application.port.in.user.UserCreateCommand;
 import com.example.hexagonalarchitecture.application.port.in.user.UserCreateUseCase;
 import com.example.hexagonalarchitecture.application.port.in.user.UserDTO;
-import com.example.hexagonalarchitecture.application.port.out.SaveUserPort;
+import com.example.hexagonalarchitecture.application.port.out.CreateUserPort;
 import com.example.hexagonalarchitecture.domain.User;
-import com.example.hexagonalarchitecture.util.AesUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 public class UserService implements UserCreateUseCase {
 
-    private final SaveUserPort saveUserPort;
-
+    private final CreateUserPort createUserPort;
     @Override
     public UserDTO create(UserCreateCommand command) {
         String validateResult = command.validate();
         if (StringUtils.hasText(validateResult)){
             return UserDTO.ofFailed(validateResult);
         }
-        saveUserPort.save(User.builder()
-            .userName(AesUtils.encrypt(command.getUserName()))
-            .phoneNumber(AesUtils.encrypt(command.getPhoneNumber()))
-            .jumin(AesUtils.encrypt(command.getJumin()))
+        createUserPort.create(User.builder()
+            .userName(command.getUserName())
+            .phoneNumber(command.getPhoneNumber())
+            .jumin(command.getJumin())
             .build());
         return UserDTO.ofSuccess("등록에 성공하였습니다");
     }
