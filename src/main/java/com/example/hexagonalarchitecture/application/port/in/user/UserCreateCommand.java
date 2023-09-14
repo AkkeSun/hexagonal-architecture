62aafa3cbc014a39379139f737af4933ee8f438d
od <od@sweettracker.co.kr>
2023-09-14 15:02:49 +0900

    perf: UserCreateCommand validate 추가

--- src/main/java/com/example/hexagonalarchitecture/application/port/in/user/UserCreateCommand.java
+++ src/main/java/com/example/hexagonalarchitecture/application/port/in/user/UserCreateCommand.java
@@ -16,6 +16,12 @@ public class UserCreateCommand {
     private String phoneNumber;
 
     public String validate(){
+        if(!jumin.matches("\\d{2}([0]\\d|[1][0-2])([0][1-9]|[1-2]\\d|[3][0-1])[-]*[1-4]\\d{6}")){
+            return "주민등록번호가 올바르지 않습니다";
+        }
+        if (phoneNumber.matches("\\d{3}-\\d{3,4}-\\d{4}")){
+            return "전화번호가 올바르지 않습니다";
+        }
         return "";
     }
 
