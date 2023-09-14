c3d21243cb9376de40a081c599e3fa9c266f296d
od <od@sweettracker.co.kr>
2023-09-14 15:28:24 +0900

    perf: UserCommandValidationTest 에 맞춰 command 수정

--- src/main/java/com/example/hexagonalarchitecture/application/port/in/user/UserCreateCommand.java
+++ src/main/java/com/example/hexagonalarchitecture/application/port/in/user/UserCreateCommand.java
@@ -1,25 +1,33 @@
 package com.example.hexagonalarchitecture.application.port.in.user;
 
-import com.sun.istack.NotNull;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.util.StringUtils;
 
 @Getter
+@Setter
 public class UserCreateCommand {
 
-    @NotNull
     private String userName;
 
-    @NotNull
     private String jumin;
 
-    @NotNull
     private String phoneNumber;
 
     public String validate(){
+        if(!StringUtils.hasText(userName)){
+            return "이름이 비어있습니다";
+        }
+        if(!StringUtils.hasText(jumin)){
+            return "주민등록번호가 비어있습니다";
+        }
+        if(!StringUtils.hasText(phoneNumber)){
+            return "전화번호가 비어있습니다";
+        }
         if(!jumin.matches("\\d{2}([0]\\d|[1][0-2])([0][1-9]|[1-2]\\d|[3][0-1])[-]*[1-4]\\d{6}")){
             return "주민등록번호가 올바르지 않습니다";
         }
-        if (phoneNumber.matches("\\d{3}-\\d{3,4}-\\d{4}")){
+        if (!phoneNumber.matches("\\d{3}-\\d{3,4}-\\d{4}")){
             return "전화번호가 올바르지 않습니다";
         }
         return "";
