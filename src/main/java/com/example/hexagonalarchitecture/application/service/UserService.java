49cf4acec8df4e00d7b86676a96631f06009e11d
od <od@sweettracker.co.kr>
2023-09-14 18:31:38 +0900

    chore: 미사용 insert문 제거

--- src/main/java/com/example/hexagonalarchitecture/application/service/UserService.java
+++ src/main/java/com/example/hexagonalarchitecture/application/service/UserService.java
@@ -8,7 +8,6 @@ import com.example.hexagonalarchitecture.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 @Slf4j
 @Service
