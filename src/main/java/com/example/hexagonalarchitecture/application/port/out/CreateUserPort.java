787a469cc70f8d9bb00ca4cd862a42c2544df58e
od <od@sweettracker.co.kr>
2023-09-14 15:03:20 +0900

    perf: CreateUserPort 개선

--- src/main/java/com/example/hexagonalarchitecture/application/port/out/CreateUserPort.java
+++ src/main/java/com/example/hexagonalarchitecture/application/port/out/CreateUserPort.java
@@ -2,6 +2,6 @@ package com.example.hexagonalarchitecture.application.port.out;
 
 import com.example.hexagonalarchitecture.domain.User;
 
-public interface SaveUserPort {
-    User save(User user);
+public interface CreateUserPort {
+    void create(User user);
 }
